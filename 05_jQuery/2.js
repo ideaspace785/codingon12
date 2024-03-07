f4b7fd5d9b3ebb499b9b72375ef28f5061a34764
ideaspace785 <nominaeri8@gmail.com>
2024-03-07 10:47:01 +0900

    study: jquery and bootstrap

---
+++ 05_jQuery/2.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("button").click(function(){
+       //$("p .intro").css("background-color", "pink");
+       //("p.intro") 공백이 없는거는 p태그의 intro 클래스를 의미 , 공백이 있는거는 p tag의 자식 요소 후손 중의 intro class 있는 애를 선택 여러개 있으면 다 선택된다. 
+
+       //$("p:first").css("background-color", "pink");
+       //$("p:last").css("background-color", "pink");
+       //:first, :last 가장 먼저 가장뒤에 있는 요소가 선택된다.  
+
+    })
+})
