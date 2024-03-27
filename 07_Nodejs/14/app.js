a7506ba3d4dabeb2ddd0ea4fb1198e50e453a8d6
ideaspace785 <121221475+ideaspace785@users.noreply.github.com>
2024-03-27 12:07:29 +0900

    Nodejs

--- 07_Nodejs/14/app.js
+++ 07_Nodejs/14/app.js
@@ -2,8 +2,10 @@ const express=require('express')
 const app= express();
 const PORT=8000;
 
+//css file 등록
+app.use(express.static('public'));
 
-
+//ejs 설정
 app.set("view engine", 'ejs');
 app.set('views', './views');
 
@@ -14,8 +16,10 @@ app.get('/about', (req, res)=>{
 
 
 app.get('/', (req,res)=>{
-    res.render("index", {title:'HOME'})
-    // rendering: 보낸코드를 HTML인지 확인하고 , 그 페이지에 필요한 css 파일 등을 요청해서 페이지를 그려준다. 
+
+    res.render("index", {title:'MYHOME'})
+    // rendering: 보낸코드를 HTML인지 확인하고 , 그 페이지에 필요한 css 파일 등을 요청해서 페이지를 그려준다.
+    //확장자 .html를 적어줄 필요가 없다. ejs로 set를 해줬으니 
 })
 app.get('/create', (req, res)=>{
     res.render("create", {title:'작성하기'})
@@ -25,4 +29,11 @@ app.use(PORT, ()=>{
 })
 app.listen(PORT, ()=>{
     console.log(`Server is running ${PORT}`)
+})
+
+
+
+//없는 경로 
+app.use((req, res)=>{
+    res.status(404).render('404')
 })
